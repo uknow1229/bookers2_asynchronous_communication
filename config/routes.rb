8da5574121a255b921ccbff3a006646634aaf713
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-04-18 15:51:20 +0000

    [Start]asynchronous_communication

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :users, only: [:index,:show,:edit,:update]
+  get '/search', to: 'searches#search'
+end
